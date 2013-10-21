494fca3b87cb0589f8a06d76fa30989f2e3adb99
mikera <mike.r.anderson.13@gmail.com>
2013-10-21 19:47:39 +0800

    add test case, update to clojure-utils snapshot

--- src/main/clojure/mikera/sensor/core.clj
+++ src/main/clojure/mikera/sensor/core.clj
@@ -1,4 +1,34 @@
 (ns mikera.sensor.core
   ;; (:require [clojure.core.matrix :as m])
+  (:require [clojure.core.async :as a :refer [go chan <! >! <!! >!!]])
+  (:require [mikera.cljutils.hex :as hex])
   (:require [serial-port :as sp]))
 
+
+(defn ble-messages 
+  "Creates a Blutooth LE listener channel that reads bytes from byte-chan
+   and returns a new value when the message is complete"
+  ([byte-chan]
+    (go )))
+
+(defn setup []
+	(def port (sp/open "COM4"))
+	
+	(sp/on-byte port #(println %))
+
+
+)
+
+(defn cleanup []
+  (sp/close port)  
+)
+
+(def print-chan
+  (let [c (chan)]
+    (go 
+      (loop []
+        (let [r1 (<! c)
+              r2 (<! c)]
+          (println (str r1 r2)))
+        (recur)))
+    c))
---
+++ src/test/java/mikera/sensor/TestMotionSensor.java
@@ -0,0 +1,11 @@
+package mikera.sensor;
+
+import mikera.cljunit.ClojureTest;
+
+public class TestMotionSensor extends ClojureTest {
+	
+	@Override
+	public String filter () {
+		return "mikera.sensor"; 
+	}
+}
