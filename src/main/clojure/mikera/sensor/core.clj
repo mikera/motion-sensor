8701e2cacca7c83ddd557b64b7361793e566913f
mikera <mike.r.anderson.13@gmail.com>
2013-10-22 01:32:08 +0800

    more code in progress

--- src/main/clojure/mikera/sensor/core.clj
+++ src/main/clojure/mikera/sensor/core.clj
@@ -2,6 +2,7 @@
   ;; (:require [clojure.core.matrix :as m])
   (:require [clojure.core.async :as a :refer [go chan <! >! <!! >!!]])
   (:require [mikera.cljutils.hex :as hex])
+  (:require [mikera.cljutils.bytes :as bytes])
   (:require [serial-port :as sp]))
 
 
@@ -32,3 +33,26 @@
           (println (str r1 r2)))
         (recur)))
     c))
+
+(defn to-bytes 
+  "Converts a message piece to a byte array."
+  ([msg]
+    (cond 
+      (instance? bytes/BYTE-ARRAY-CLASS msg) msg
+      (string? msg) (hex/bytes-from-hex-string msg)
+      (keyword? msg) (hex/bytes-from-hex-string (name msg))
+      :else (hex/bytes-from-hex-string (hex/hex-string msg))))
+  ([msg & msgs]
+    (loop [bytes (to-bytes msg)
+           msgs (seq msgs)]
+      (if msgs
+        (let [msg (first msgs)]
+          (recur (bytes/join-bytes bytes (to-bytes msg)) 
+                 (next msgs)))
+        bytes))))
+
+(defn send-msg [port & msgs]
+  (sp/write port (apply to-bytes msgs)))
+
+(defn connect [bda-str]
+  (send ()))
